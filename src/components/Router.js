25f5a103d288f4ef9999bc211a638c3acde54812
yesaroun <akor1@naver.com>
2023-02-03 11:22:52 +0900

    로그인된 경우와 로그인 되지 않은 경우 분기 처리

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Home from "../routes/Home"
 import Auth from "../routes/Auth"
 import Profile from "../routes/Profile";
 
 const AppRouter = ({isLoggedIn, userObj}) => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/profile" element={<Profile />}/>
-                <Route path="/auth" element={<Auth />}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        {isLoggedIn && <div>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </div>}
+        {!isLoggedIn && <div>
+          <Route path="/auth" element={<Auth/>}/>
+        </div>}
+        {/*로그인 된 경우 홈 화면으로  보내고 그렇지 않은 경우 auth로 보낸다.*/}
+      </Routes>
+    </Router>
+  );
 }
 
 export default AppRouter
