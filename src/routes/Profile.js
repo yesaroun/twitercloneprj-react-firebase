992196221ea0854c564d820e4e2ef0f634d8163f
yesaroun <akor1@naver.com>
2023-02-06 13:50:34 +0900

    logout 처리

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Profile = () => {
   const navigate = useNavigate();
   const onLogOutClick = () => {
+    // console.log("logout click");   // testcode
     auth.signOut();
     navigate("/");
   };
@@ -14,5 +15,12 @@ const Profile = () => {
     </>
   );
 };
+// 로그아웃 처리 코드
+/*
+  jsx 문법은 무조건 상단에 <div>나 빈태그(<></>)를 넣는다.
+  코딩할 때는 화면을 먼저 그리고(return)
+  여기에 기능을 준다. onclick에서 onLogOutClick() 추가
+
+*/
 
 export default Profile
